d7c3892ef3dad6ea66057c5ec1495a060e5e702b
manish <mgupta9151@gmail.com>
2019-01-30 23:48:31 +0530

    added validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_many :bookings , dependent: :destroy
+    validates :first_name,:last_name,:age,:contact_number , presence: :true
 end
